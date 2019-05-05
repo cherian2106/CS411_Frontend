d9c4dc25dc63be5cd057d2f3d69daec91051ffee
GongCode <jasonxgong@gmail.com>
2019-05-04 20:47:12 -0500

    Login page looks nice now

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,6 @@ import TextField from 'material-ui/TextField';
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import login from './Login.module.scss'
-import Auth from '../../config.js'
 
 
 class Login extends Component {
@@ -18,14 +17,33 @@ constructor(props){
  }
 
  LoginClick = () =>{
-    Auth.signInWithEmailAndPassword(this.state.username, this.state.password)
-    .then(function(result) {
-      console.log(result)
-      window.location.href = process.env.PUBLIC_URL+"/search";
-    })
-    .catch(function(err){
-      window.alert(err.code);
-    });
+  let config = {
+    apiKey: "AIzaSyBaoiWVV8vuD8uiRI3AiFEvC3xCs8m2MTY",
+    authDomain: "cs411-c57f9.firebaseapp.com",
+    databaseURL: "https://cs411-c57f9.firebaseio.com",
+    projectId: "cs411-c57f9",
+    storageBucket: "cs411-c57f9.appspot.com",
+    messagingSenderId: "259780274004"
+  };
+  firebase.initializeApp(config);
+  const auth = firebase.auth()
+  const res = auth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
+  .then(function() {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return auth.signInWithEmailAndPassword(this.state.username, this.state.password)
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+  });
+  
+  res.then(window.location.href = process.env.PUBLIC_URL+"/Search").catch(e => console.log(e.message))
 }
 
 render() {
@@ -35,8 +53,8 @@ render() {
           <div>
             <TextField
              className = {login.user}
-             hintText="Enter your Email"
-             floatingLabelText="Email"
+             hintText="Enter your Username"
+             floatingLabelText="Username"
              onChange = {(event,newValue) => this.setState({username:newValue})}
              />
              <TextField
@@ -46,8 +64,9 @@ render() {
                floatingLabelText="Password"
                onChange = {(event,newValue) => this.setState({password:newValue})}
                />
-               <br/>
+               &nbsp;
              <RaisedButton onClick = {this.LoginClick} className = {login.submit} label="Login" primary={true}/>
+           
          </div>
          </MuiThemeProvider>
       </div>
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -12,17 +12,28 @@ class LoginPage extends Component {
   }
     render() {
       return (
+
           <div className = {loginpage.loginpage}>
             <div className = {loginpage.bar}>
+              <div className = {loginpage.title}>KONNECT</div>
+              <img className = {loginpage.image1} alt = "logo" src = "https://pbs.twimg.com/profile_images/813432457382875136/gNuIo1-P.jpg"></img>
                 <Login className = {loginpage.login}></Login>
-                <img className = {loginpage.image} alt = "logo" src = "https://c1.sfdcstatic.com/content/dam/blogs/ca/Blog%20Posts/The%20Most%20Important%20Factors%20for%20Startup%20Success_Open%20Graph%20Image.png"></img>
             </div>
+            <img className = {loginpage.image} alt = "logo" src = "https://c1.sfdcstatic.com/content/dam/blogs/ca/Blog%20Posts/The%20Most%20Important%20Factors%20for%20Startup%20Success_Open%20Graph%20Image.png"></img>
+
+          <div className = {loginpage.block}>
+            <img className = {loginpage.image2} alt = "logo" src = "https://i.pinimg.com/originals/15/82/b6/1582b643a5f0f4ea3546fa220f65db3e.png"></img>
           <div className = {loginpage.signup}>
             <SignUp></SignUp>
           </div>
+          <img className = {loginpage.image3} alt = "logo" src = "https://www.shareicon.net/data/2016/01/15/703724_people_512x512.png"></img>
+          </div>
+
           </div>
       )
     }
 }
+
+
 
 export default LoginPage;
